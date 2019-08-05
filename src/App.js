7ead8230c73bde52ef289315a4a121298b385630
ghehd231 <ghehd231@naver.com>
2019-08-05 21:13:11 +0900

    Life-cicle

--- src/App.js
+++ src/App.js
@@ -7,14 +7,30 @@ class App extends Component {
   };
 
   add = () => {
-    console.log("add");
+    this.setState(current => ({ count: current.count + 1 }));
   }
 
   minus = () => {
-    console.log("minus");
+    this.setState(current => ({ count: current.count - 1 }));
   }
 
+  componentDidMount(){
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate(){
+    console.log("componentDidUpdate");
+  }
+
+  componentWillUnmount(){
+    console.log("componentWillUnmount");
+  }
+
+
+
+
   render(){
+    console.log("I'm rendering ");
     return(
       <div>
         <h1>The number is : {this.state.count}</h1>
